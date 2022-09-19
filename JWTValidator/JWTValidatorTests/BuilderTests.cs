776ef98275cddd475c411cf29a4f9b69f69c01a1
Oliver Bourne <otgb97@gmail.com>
2022-09-19 19:53:54 +0100

    add buider tests

--- JWTValidator/JWTValidatorTests/BuilderTests.cs
+++ JWTValidator/JWTValidatorTests/BuilderTests.cs
@@ -16,11 +16,45 @@ public class BuilderTests
         JWTValidatorOptions jWTValidatorOptions = JWTValidatorOptionsBuilderCreator
             .Create()
             .WithSigningKeyFromSecret("abc")
-            .WithIssuer("a")
-            .WithExpiryDate()
             .Build();
 
         //Assert
         Assert.AreEqual("abc", jWTValidatorOptions.Secret);
+    }
+
+    [TestMethod]
+    public void BasicBuilderUrlTests()
+    {
+        //Associate
+
+        //Act
+        JWTValidatorOptions jWTValidatorOptions = JWTValidatorOptionsBuilderCreator
+            .Create()
+            .WithSigningKeyFromOpenIdUrl("https://helloworld.com")
+            .Build();
+
+        //Assert
+        Assert.AreEqual("https://helloworld.com", jWTValidatorOptions.OpenIdUrl);
+    }
+
+    [TestMethod]
+    public void ComplexBuilderTests()
+    {
+        //Associate
+
+        //Act
+        JWTValidatorOptions jWTValidatorOptions = JWTValidatorOptionsBuilderCreator
+            .Create()
+            .WithSigningKeyFromOpenIdUrl("https://helloworld.com")
+            .WithAudience("hello")
+            .WithExpiryDate()
+            .WithIssuer("world")
+            .Build();
+
+        //Assert
+        Assert.AreEqual("https://helloworld.com", jWTValidatorOptions.OpenIdUrl);
+        Assert.AreEqual("", jWTValidatorOptions.Audience);
+        Assert.IsTrue(jWTValidatorOptions.ExpiryDate);
+        Assert.AreEqual("", jWTValidatorOptions.Issuer);
     }
 }
