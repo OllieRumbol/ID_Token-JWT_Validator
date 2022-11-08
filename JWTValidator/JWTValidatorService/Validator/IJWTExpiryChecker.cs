360880256e8b60fe219508509192a3304b222731
Oliver Bourne <otgb97@gmail.com>
2022-11-08 21:51:19 +0000

    Adding feature to validate if a JWT has expired

---
+++ JWTValidator/JWTValidatorService/Validator/IJWTExpiryChecker.cs
@@ -0,0 +1,20 @@
+﻿namespace JWTValidatorService.Validator;
+
+internal interface IJWTExpiryChecker
+{
+    Boolean HasJWTExpired(String jwt, String signingKey);
+
+    Boolean HasJWTExpired(String jwt, Uri openIdUrl);
+
+    Boolean TryHasJWTExpired(String jwt, String signingKey, out Boolean isExpired);
+
+    Boolean TryHasJWTExpired(String jwt, Uri openIdUrl, out Boolean isExpired);
+
+    DateTime WhenDoesJWTExpire(String jwt, String signingKey);
+
+    DateTime WhenDoesJWTExpire(String jwt, Uri openIdUrl);
+
+    Boolean TryGetWhenDoesJWTExpire(String jwt, String signingKey, out DateTime expiryDate);
+
+    Boolean TryGetWhenDoesJWTExpire(String jwt, Uri openIdUrl, out DateTime expiryDate);
+}
